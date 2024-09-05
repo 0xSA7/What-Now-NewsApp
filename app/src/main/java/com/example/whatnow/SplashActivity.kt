953da4fdabc01e7546e73507d0d07cdbf5af2673
eyadamr905 <eyadamro905@gmail.com>
2024-09-05 16:47:39 +0300

    added splash scree & onboarding &bug fixed

---
+++ app/src/main/java/com/example/whatnow/SplashActivity.kt
@@ -0,0 +1,56 @@
+package com.example.whatnow
+
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_DISPLAY_LENGTH: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val enterAnimation = AnimationUtils.loadAnimation(this, R.anim.img_enter_animation)
+        val exitAnimation = AnimationUtils.loadAnimation(this, R.anim.exit_animation)
+        val rootLayout = findViewById<View>(R.id.splash)
+
+        val imageView = findViewById<ImageView>(R.id.splashImage)
+        imageView.startAnimation(enterAnimation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val sharedPreferences: SharedPreferences =
+                getSharedPreferences("onboarding_prefs", MODE_PRIVATE)
+            val isFirstTime: Boolean = sharedPreferences.getBoolean("isFirstTime", true)
+
+            Log.d("SplashActivity", "isFirstTime: $isFirstTime")
+
+            val intent = if (isFirstTime) {
+                Log.d("SplashActivity", "Redirecting to Onboarding")
+                Intent(this, Onboarding::class.java)
+            } else {
+                Log.d("SplashActivity", "Redirecting to SignupActivity")
+                Intent(this, SignupActivity::class.java)
+            }
+
+            Log.d("SplashActivity", "Starting intent: ${intent.component?.className}")
+
+
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                startActivity(intent)
+                overridePendingTransition(R.anim.img_enter_animation, R.anim.exit_animation)
+                finish()
+            }, exitAnimation.duration)
+
+        }, SPLASH_DISPLAY_LENGTH)
+    }
+}
