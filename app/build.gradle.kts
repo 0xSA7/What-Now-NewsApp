3e759f0066addc4ccbcfcc949ba3c28054c1bbf4
0xSA7 <salehdiaaahmed@gmail.com>
2024-09-19 22:35:09 +0300

    final commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "2.0"
         android.buildFeatures.buildConfig = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
